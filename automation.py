a0d49892da7cbb63000ed12d09abc1d0570c6ef2
Xin-tong-Jiang <155217760+Xin-tong-Jiang@users.noreply.github.com>
2025-09-26 21:56:11 -0700

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,164 @@
+from reddit_scraper import init_db, main
+import os
+import mysql.connector
+import numpy as np
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import nltk
+from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import chromadb
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+
+# Chromadb Configuration
+COLLECTION_NAME = "Vacation_Texts_and_Vectors"
+client = chromadb.Client()
+LAST_ID = 0
+
+# MySQL Configuration
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "phpmyadmin")
+DB_PASS = os.getenv("DB_PASS", "root")
+DB_NAME = os.getenv("DB_NAME", "reddit_scraper")
+TABLE_NAME = "posts"
+
+def to_text(x):
+    if x is None:
+        return ""
+    if isinstance(x, (list, tuple, set)):
+        return " ".join(map(str, x))
+    if isinstance(x, dict):
+        return " ".join(f"{k}:{v}" for k, v in x.items())
+    return str(x)
+
+
+def load_raw_texts():
+    # Read text from MySQL
+    conn = mysql.connector.connect(host=DB_HOST, 
+                                   port=DB_PORT, 
+                                   user=DB_USER, 
+                                   password=DB_PASS,
+                                   database=DB_NAME)
+    cur = conn.cursor(dictionary=True)
+    cur.execute(f"SELECT * FROM {TABLE_NAME}")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return rows
+
+def train_doc2vec(data):
+    # preproces the documents, and create TaggedDocuments
+    tagged_data = [TaggedDocument(words=word_tokenize(doc.lower()), tags=[str(i)]) for i, doc in enumerate(data)]
+
+    # train the Doc2vec model
+    model = Doc2Vec(vector_size=100, min_count=1, epochs=100)
+
+    model.build_vocab(tagged_data)
+    model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
+
+    # get the document vectors
+    document_vectors = [model.infer_vector(word_tokenize(doc.lower())) for doc in data]
+    
+    return model, document_vectors
+
+
+def infer_vectors(model, text):
+    vector = model.infer_vector(word_tokenize(text.lower()))
+    return np.asanyarray(vector, dtype="float32")
+
+
+def store_document_vectors(documents, document_vectors):
+    # create a collection
+    collection = client.get_or_create_collection(
+        name=COLLECTION_NAME,
+        metadata={"description": "store raw texts and embeddings of vacation reddit posts"}
+    )
+
+    # write raw texts and vectors
+    ids = [f"doc-{i}" for i in range(len(documents))]
+    metadatas = [{"source": "Reddit"} for _ in documents]
+    global LAST_ID
+    LAST_ID = ids
+
+    collection.add(
+        ids=ids,
+        documents=documents,
+        embeddings=document_vectors.tolist(),
+        metadatas=metadatas
+    )
+
+
+def store_new_vector(text, vector):
+    # delete old collection if exists
+    existing_collections = [c.name for c in client.list_collections()]
+    if COLLECTION_NAME in existing_collections:
+        client.delete_collection(name=COLLECTION_NAME)
+
+    # create a collection
+    collection = client.get_or_create_collection(
+        name=COLLECTION_NAME
+    )
+
+    # write new text and vector
+    global LAST_ID
+    LAST_ID += 1
+    new_id = f"doc-{LAST_ID}"
+    metadata = {"source": "Reddit"}
+
+    collection.add(
+        ids=[new_id],
+        documents=[text],
+        embeddings=[vector],
+        metadatas=[metadata]
+    )
+
+    return new_id
+
+def document_clustering():
+    # get documents and vectors
+    collection = client.get_or_create_collection(name=COLLECTION_NAME)
+    result = collection.get(
+        include=["ids", "documents", "embeddings"]
+    )
+    ids = result["ids"]
+    documents = result["documents"]
+    embeddings = result["embeddings"]
+
+    ######## PUT CLUSTERING CODE HERE
+
+
+def new_text_clustering(vector):
+    pass
+    ######## PUT CLUSTERING CODE HERE
+
+
+if __name__=='__main__':
+    # fetch data
+    init_db()
+    main()
+
+    # read data from MySQL
+    rows = load_raw_texts()
+    data = [
+        " ".join(filter(None, [
+            to_text(row.get("title")),
+            to_text(row.get("keywords")),
+            to_text(row.get("body")),
+            to_text(row.get("images")),
+        ])).strip()
+        for row in rows
+    ]
+
+    model, document_vectors = train_doc2vec(data)
+    store_new_vector(data, document_vectors)
+    document_clustering()
+
+    new_data = "I like Hawaii"
+    vector = infer_vectors(model, new_data)
+
+    new_id = store_new_vector(new_data, vector)
+    # new_text_clustering(vector)
